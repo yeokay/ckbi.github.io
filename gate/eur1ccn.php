c1f730d684a59a4f2e8190db617c3106123909db
akame <87499021+ckbi@users.noreply.github.com>
2023-07-30 13:36:43 +0200

    Update eur1ccn.php

--- gate/eur1ccn.php
+++ gate/eur1ccn.php
@@ -33,7 +33,7 @@ if(empty($amt)) {
 	$amt = '1';
 	$chr = $amt * 100;
 }
-$sk = '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';
+$sk = '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';
 
 $lista = $_GET['lista'];
     $cc = multiexplode(array(":", "|", ""), $lista)[0];
