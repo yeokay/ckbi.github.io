db4e0a516d5edac69b8842f1d0b39f768999ca0a
akame <87499021+ckbi@users.noreply.github.com>
2023-07-30 11:37:00 +0200

    Update usd1ccn.php

--- gate/usd1ccn.php
+++ gate/usd1ccn.php
@@ -33,7 +33,7 @@ if(empty($amt)) {
 	$amt = '1';
 	$chr = $amt * 100;
 }
-$sk = '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';
+$sk = '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';
 
 $lista = $_GET['lista'];
     $cc = multiexplode(array(":", "|", ""), $lista)[0];
