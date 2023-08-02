4385009323eec46fa900fa27986cfbaa20f22aed
akame <87499021+ckbi@users.noreply.github.com>
2023-08-02 17:21:00 +0200

    Update eur1ccn.php

--- gate/eur1ccn.php
+++ gate/eur1ccn.php
@@ -33,7 +33,7 @@ if(empty($amt)) {
 	$amt = '0.5';
 	$chr = $amt * 100;
 }
-$sk = 'sk_live_3AsmsZEXRTqT7eY3njH68FDa';
+$sk = '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';
 
 $lista = $_GET['lista'];
     $cc = multiexplode(array(":", "|", ""), $lista)[0];
