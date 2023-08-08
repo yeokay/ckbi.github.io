b95c1466b3c3ff628382e6587e810b019367ea13
akame <87499021+ckbi@users.noreply.github.com>
2023-08-08 10:27:52 +0200

    Update usd10ccn.php

--- gate/usd10ccn.php
+++ gate/usd10ccn.php
@@ -33,7 +33,7 @@ if(empty($amt)) {
 	$amt = '10';
 	$chr = $amt * 100;
 }
-$sk = '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';
+$sk = 'sk_live_SH3IujFg0gV5UacbZYICGZXj';
 
 $lista = $_GET['lista'];
     $cc = multiexplode(array(":", "|", ""), $lista)[0];
