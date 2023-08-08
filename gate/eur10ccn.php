b9356e211249610b7dc502624d58ee28fffae0de
akame <87499021+ckbi@users.noreply.github.com>
2023-08-08 07:54:40 +0200

    Update eur10ccn.php

--- gate/eur10ccn.php
+++ gate/eur10ccn.php
@@ -33,7 +33,7 @@ if(empty($amt)) {
 	$amt = '10';
 	$chr = $amt * 100;
 }
-$sk = 'sk_test_51N8rjjCPPYlsE6nN1mfm3qGlmHm0pYsv0mzU2nl4XJeu68Shl2LHhHILVQnsWbWJAjCZvRrtO9fIo0dXFSnjZYP000QoJazVo1_';
+$sk = 'sk_test_51N8rjjCPPYlsE6nN1mfm3qGlmHm0pYsv0mzU2nl4XJeu68Shl2LHhHILVQnsWbWJAjCZvRrtO9fIo0dXFSnjZYP000QoJazVo1';
 
 $lista = $_GET['lista'];
     $cc = multiexplode(array(":", "|", ""), $lista)[0];
