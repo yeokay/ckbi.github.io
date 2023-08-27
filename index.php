03093ba0354b8fafe55161ceabd944645e16b89e
yeokay <136389261+yeokay@users.noreply.github.com>
2023-08-28 00:38:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -209,6 +209,7 @@ if(isset($_POST['but_logout'])){
 							  <option style="background:rgba(165, 154, 154, 0.281);color:rgb(255, 208, 0);color:black" value="gate/ccn100usd.php">𝒄𝒄𝒏 𝟏𝟎0$</option>
 							  <option style="background:rgba(165, 154, 154, 0.281);color:rgb(255, 208, 0);color:black" value="gate/ccn50usd.php">𝒄𝒄𝒏 50$</option>
                  <option style="background:rgba(165, 154, 154, 0.281);color:rgb(255, 208, 0);color:black" value="gate/usd1cvv.php">𝐜𝐯𝐯 𝟏$</option>
+							  							  <option style="background:rgba(165, 154, 154, 0.281);color:rgb(255, 208, 0);color:black" value="gate/nw.php"></option>
 </select>
 	<br>										
 					<button class="btn btn-play  btn-bg-gradient text-white" style="width: 45%; float: left; "><i class="fa fa-play" style="margin-right: 3px;"></i>𝒄𝒉𝒌 </button>
